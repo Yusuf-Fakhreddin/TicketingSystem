904e5e7c6b7a206adc4ce6c999602e21a6416729
Yusuf Fakhreddin <48655310+Yusuf-Fakhreddin@users.noreply.github.com>
2024-01-27 11:33:44 +0200

    Revert "Ticket Full CRUD"
    This reverts commit 075163fee91ecb734e3476faa1d077620c489246.

--- SQL/Comment.sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE `ticket_comments` (
-  `id` INT AUTO_INCREMENT PRIMARY KEY,
-  `ticket_id` INT,
-  `comment` VARCHAR(255),
-  FOREIGN KEY (`ticket_id`) REFERENCES `ticket`(`id`)
-);
--- src/main/java/com/YusufFakhreddin/ICDTicketingSystem/dao/TicketRepo.java
+++
@@ -1,7 +0,0 @@
-package com.YusufFakhreddin.ICDTicketingSystem.dao;
-import com.YusufFakhreddin.ICDTicketingSystem.entity.Ticket;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface TicketRepo extends JpaRepository<Ticket, String> {
-
-}
